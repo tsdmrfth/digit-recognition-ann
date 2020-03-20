cf30bf38fcc2fff1a5a4efb7069b7eb84794ef42
Fatih Tasdemir <tsdmrfth@gmail.com>
2020-03-20 14:35:22 +0300

    Updated main.py

--- server/api/main.py
+++ server/api/main.py
@@ -2,6 +2,7 @@ import io
 
 from PIL import Image
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 from numpy import asarray
 from sklearn.datasets import load_digits
 from starlette.responses import JSONResponse
@@ -11,6 +12,18 @@ from server.model.digit_recognizer import DigitRecognizerNN
 app = FastAPI()
 
 model = DigitRecognizerNN()
+
+origins = [
+    'http://192.168.1.100:19006'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get('/')
@@ -27,15 +40,21 @@ def train_model():
     return 'done'
 
 
-@app.get('/prediction')
+@app.post('/prediction')
 async def get_prediction(digit_file: UploadFile = File(...)):
+    status_code = 200
+
     if model.is_trained:
         image_data = await digit_file.read()
         image = Image.open(io.BytesIO(image_data))
         data = asarray(image)
-        return int(model.predict(data[0:, 0:8, :1].reshape(64)))
+        prediction = int(model.predict(data[0:, 0:8, :1].reshape(64)))
+        response_content = {'prediction': prediction}
     else:
-        return JSONResponse(content={'error': 'Model should be trained.'}, status_code=401)
+        response_content = {'error': 'Model should be trained. Training...'}
+        status_code = 403
+
+    return JSONResponse(content=response_content, status_code=status_code)
 
 
 if __name__ == "__main__":
