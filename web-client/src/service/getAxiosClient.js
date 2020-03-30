53d2d5df44101199e32c5a7393ac73b079f616d0
Fatih Tasdemir <tsdmrfth@gmail.com>
2020-03-30 22:46:47 +0300

    Changed server url.

--- web-client/src/service/getAxiosClient.js
+++ web-client/src/service/getAxiosClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://127.0.0.1:5000/'
+const baseURL = 'http://127.0.0.1:8000/'
 export const getAxiosClient = () => axios.create({
     baseURL,
     withCredentials: true,
